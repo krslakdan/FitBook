8bcfb0d892a91ff19564910c80a1dec701390746
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-02 21:02:05 +0200

    Remove reflection

--- FitBook.Services/BaseCRUDService.cs
+++ FitBook.Services/BaseCRUDService.cs
@@ -3,42 +3,60 @@ using FitBook.Model.SearchObjects;
 using FitBook.Services.Database;
 using FitBook.Services.Database.Entities;
 using FitBook.Services.Interfaces;
-using Mapster;
+using FluentValidation;
 using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FitBook.Services;
 
 public abstract class BaseCRUDService<TEntity, TResponse, TSearch, TInsertRequest, TUpdateRequest>
     : BaseReadService<TEntity, TResponse, TSearch>,
       IBaseCRUDService<TResponse, TSearch, TInsertRequest, TUpdateRequest>
-    where TEntity : class
+    where TEntity : BaseEntity
     where TSearch : BaseSearchObject, new()
 {
-    protected BaseCRUDService(FitBookDbContext dbContext, IMapper mapper)
-        : base(dbContext, mapper)
+    private readonly IValidator<TInsertRequest> _insertValidator;
+    private readonly IValidator<TUpdateRequest> _updateValidator;
+
+    protected BaseCRUDService(
+        FitBookDbContext dbContext,
+        IMapper mapper,
+        ILoggerFactory loggerFactory,
+        IValidator<TInsertRequest> insertValidator,
+        IValidator<TUpdateRequest> updateValidator)
+        : base(dbContext, mapper, loggerFactory)
     {
+        _insertValidator = insertValidator;
+        _updateValidator = updateValidator;
     }
 
     public virtual async Task<TResponse> InsertAsync(TInsertRequest request, CancellationToken cancellationToken = default)
     {
+        await _insertValidator.ValidateAndThrowAsync(request, cancellationToken);
         await ValidateInsert(request, cancellationToken);
 
         var entity = MapInsertToEntity(request);
+        ApplyInsertDefaults(entity);
         await BeforeInsert(request, entity, cancellationToken);
 
-        DbContext.Set<TEntity>().Add(entity);
-        await DbContext.SaveChangesAsync(cancellationToken);
+        _dbContext.Set<TEntity>().Add(entity);
+        await _dbContext.SaveChangesAsync(cancellationToken);
 
         await AfterInsert(entity, cancellationToken);
-        return entity.Adapt<TResponse>();
+        _logger.LogInformation(
+            "Inserted {EntityType} with id {EntityId}",
+            typeof(TEntity).Name,
+            entity.Id);
+
+        return _mapper.Map<TResponse>(entity);
     }
 
     public virtual async Task<TResponse> UpdateAsync(int id, TUpdateRequest request, CancellationToken cancellationToken = default)
     {
-        var entity = await BuildWriteQuery()
-            .FirstOrDefaultAsync(item => EF.Property<int>(item, "Id") == id, cancellationToken);
+        await _updateValidator.ValidateAndThrowAsync(request, cancellationToken);
 
+        var entity = await FindWriteEntityByIdAsync(id, cancellationToken);
         if (entity is null)
         {
             throw new NotFoundException($"{typeof(TEntity).Name} with id {id} was not found.");
@@ -48,17 +66,21 @@ public abstract class BaseCRUDService<TEntity, TResponse, TSearch, TInsertReques
         await BeforeUpdate(id, request, entity, cancellationToken);
 
         MapUpdateToEntity(request, entity);
-        await DbContext.SaveChangesAsync(cancellationToken);
+        entity.UpdatedAtUtc = DateTime.UtcNow;
+        await _dbContext.SaveChangesAsync(cancellationToken);
 
         await AfterUpdate(id, request, entity, cancellationToken);
-        return entity.Adapt<TResponse>();
+        _logger.LogInformation(
+            "Updated {EntityType} with id {EntityId}",
+            typeof(TEntity).Name,
+            id);
+
+        return _mapper.Map<TResponse>(entity);
     }
 
     public virtual async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
     {
-        var entity = await BuildWriteQuery()
-            .FirstOrDefaultAsync(item => EF.Property<int>(item, "Id") == id, cancellationToken);
-
+        var entity = await FindWriteEntityByIdAsync(id, cancellationToken);
         if (entity is null)
         {
             throw new NotFoundException($"{typeof(TEntity).Name} with id {id} was not found.");
@@ -70,73 +92,65 @@ public abstract class BaseCRUDService<TEntity, TResponse, TSearch, TInsertReques
         if (entity is ISoftDeletable softDeletableEntity)
         {
             softDeletableEntity.IsDeleted = true;
+            entity.UpdatedAtUtc = DateTime.UtcNow;
         }
         else
         {
-            DbContext.Set<TEntity>().Remove(entity);
+            _dbContext.Set<TEntity>().Remove(entity);
         }
 
-        await DbContext.SaveChangesAsync(cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
         await AfterDelete(id, entity, cancellationToken);
+        _logger.LogInformation(
+            "Deleted {EntityType} with id {EntityId}",
+            typeof(TEntity).Name,
+            id);
     }
 
     protected virtual IQueryable<TEntity> BuildWriteQuery()
     {
-        return BuildQuery();
+        return ApplyQueryPipeline(BuildQuery(), new TSearch(), applySearch: false);
+    }
+
+    protected virtual Task<TEntity?> FindWriteEntityByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        return BuildWriteQuery()
+            .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
     }
 
     protected virtual TEntity MapInsertToEntity(TInsertRequest request)
     {
-        return request.Adapt<TEntity>();
+        ArgumentNullException.ThrowIfNull(request);
+        return _mapper.Map<TEntity>(request);
     }
 
     protected virtual void MapUpdateToEntity(TUpdateRequest request, TEntity entity)
     {
-        request.Adapt(entity);
+        _mapper.Map(request, entity);
     }
 
-    protected virtual Task ValidateInsert(TInsertRequest request, CancellationToken cancellationToken)
+    protected virtual void ApplyInsertDefaults(TEntity entity)
     {
-        return Task.CompletedTask;
+        if (entity.CreatedAtUtc == default)
+        {
+            entity.CreatedAtUtc = DateTime.UtcNow;
+        }
+
+        entity.UpdatedAtUtc = null;
+
+        if (entity is ISoftDeletable softDeletableEntity)
+        {
+            softDeletableEntity.IsDeleted = false;
+        }
     }
 
-    protected virtual Task ValidateUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task ValidateDelete(int id, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task BeforeInsert(TInsertRequest request, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task AfterInsert(TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task BeforeUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task AfterUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task BeforeDelete(int id, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-
-    protected virtual Task AfterDelete(int id, TEntity entity, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
+    protected virtual Task ValidateInsert(TInsertRequest request, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task ValidateUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task ValidateDelete(int id, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task BeforeInsert(TInsertRequest request, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task AfterInsert(TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task BeforeUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task AfterUpdate(int id, TUpdateRequest request, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task BeforeDelete(int id, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
+    protected virtual Task AfterDelete(int id, TEntity entity, CancellationToken cancellationToken) => Task.CompletedTask;
 }
--- FitBook.Services/BaseReadService.cs
+++ FitBook.Services/BaseReadService.cs
@@ -1,83 +1,101 @@
-using System.Linq.Dynamic.Core;
-using System.Linq.Expressions;
-using FitBook.Model.Responses;
-using FitBook.Model.Enums;
 using FitBook.Model.Exceptions;
+using FitBook.Model.Responses;
 using FitBook.Model.SearchObjects;
 using FitBook.Services.Database;
+using FitBook.Services.Database.Entities;
 using FitBook.Services.Interfaces;
-using Mapster;
 using MapsterMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace FitBook.Services;
 
 public abstract class BaseReadService<TEntity, TResponse, TSearch> : IBaseReadService<TResponse, TSearch>
-    where TEntity : class
+    where TEntity : BaseEntity
     where TSearch : BaseSearchObject, new()
 {
-    protected readonly FitBookDbContext DbContext;
-    protected readonly IMapper Mapper;
+    protected readonly FitBookDbContext _dbContext;
+    protected readonly IMapper _mapper;
+    protected readonly ILogger _logger;
 
-    protected BaseReadService(FitBookDbContext dbContext, IMapper mapper)
+    protected BaseReadService(FitBookDbContext dbContext, IMapper mapper, ILoggerFactory loggerFactory)
     {
-        DbContext = dbContext;
-        Mapper = mapper;
+        _dbContext = dbContext;
+        _mapper = mapper;
+        _logger = loggerFactory.CreateLogger(GetType());
     }
 
     public virtual async Task<TResponse> GetByIdAsync(int id, CancellationToken cancellationToken = default)
     {
         var search = new TSearch();
-        var query = BuildQuery();
-        query = AddInclude(query, search);
-        query = AddFilter(query, search);
-        query = query.AsNoTracking().Where(entity => EF.Property<int>(entity, "Id") == id);
+        var query = ApplyQueryPipeline(BuildQuery(), search, applySearch: false);
 
-        var item = await query
-            .ProjectToType<TResponse>()
-            .FirstOrDefaultAsync(cancellationToken);
+        var entity = await query
+            .AsNoTracking()
+            .FirstOrDefaultAsync(entity => entity.Id == id, cancellationToken);
 
-        if (item is null)
+        if (entity is null)
         {
             throw new NotFoundException($"{typeof(TEntity).Name} with id {id} was not found.");
         }
 
-        return item;
+        return _mapper.Map<TResponse>(entity);
     }
 
-    public virtual async Task<FitBook.Model.Responses.PagedResult<TResponse>> GetPagedAsync(TSearch? search = null, CancellationToken cancellationToken = default)
+    public virtual async Task<PageResult<TResponse>> GetAllAsync(TSearch? search = null, CancellationToken cancellationToken = default)
     {
         var searchObject = search ?? new TSearch();
-        var query = BuildQuery();
-
-        query = AddInclude(query, searchObject);
-        query = AddFilter(query, searchObject);
-        query = ApplySearch(query, searchObject);
-        query = query.AsNoTracking();
-
-        var totalCount = await query.CountAsync(cancellationToken);
+        var query = ApplyQueryPipeline(BuildQuery(), searchObject, applySearch: true);
         query = ApplySorting(query, searchObject);
 
         var skip = (searchObject.Page - 1) * searchObject.PageSize;
-        var items = await query
+        var entities = await query
+            .AsNoTracking()
             .Skip(skip)
             .Take(searchObject.PageSize)
-            .ProjectToType<TResponse>()
             .ToListAsync(cancellationToken);
 
-        return new FitBook.Model.Responses.PagedResult<TResponse>
+        int? totalCount = null;
+        int? totalPages = null;
+
+        if (searchObject.IncludeTotalCount == true)
+        {
+            var countQuery = ApplyQueryPipeline(BuildQuery(), searchObject, applySearch: true);
+            totalCount = await countQuery.CountAsync(cancellationToken);
+            totalPages = totalCount == 0
+                ? 0
+                : (int)Math.Ceiling(totalCount.Value / (double)searchObject.PageSize);
+        }
+
+        return new PageResult<TResponse>
         {
             Page = searchObject.Page,
             PageSize = searchObject.PageSize,
             TotalCount = totalCount,
-            TotalPages = totalCount == 0 ? 0 : (int)Math.Ceiling(totalCount / (double)searchObject.PageSize),
-            Items = items
+            TotalPages = totalPages,
+            Items = entities.Select(entity => _mapper.Map<TResponse>(entity)).ToList()
         };
     }
 
     protected virtual IQueryable<TEntity> BuildQuery()
     {
-        return DbContext.Set<TEntity>().AsQueryable();
+        return _dbContext.Set<TEntity>().AsQueryable();
+    }
+
+    protected IQueryable<TEntity> ApplyQueryPipeline(
+        IQueryable<TEntity> query,
+        TSearch search,
+        bool applySearch)
+    {
+        query = AddInclude(query, search);
+        query = ApplyFilter(query, search);
+
+        if (applySearch)
+        {
+            query = ApplySearch(query, search);
+        }
+
+        return query;
     }
 
     protected virtual IQueryable<TEntity> AddInclude(IQueryable<TEntity> query, TSearch search)
@@ -85,146 +103,28 @@ public abstract class BaseReadService<TEntity, TResponse, TSearch> : IBaseReadSe
         return query;
     }
 
-    protected virtual IQueryable<TEntity> AddFilter(IQueryable<TEntity> query, TSearch search)
+    protected virtual IQueryable<TEntity> ApplyFilter(IQueryable<TEntity> query, TSearch search)
     {
         return query;
     }
 
-    protected virtual IReadOnlyDictionary<string, string> BuildSortMappings()
-    {
-        return new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
-    }
-
     protected virtual IQueryable<TEntity> ApplySearch(IQueryable<TEntity> query, TSearch search)
     {
-        if (string.IsNullOrWhiteSpace(search.Search))
-        {
-            return query;
-        }
-
-        var stringProperties = typeof(TEntity)
-            .GetProperties()
-            .Where(property => property.PropertyType == typeof(string))
-            .ToArray();
-
-        if (stringProperties.Length == 0)
-        {
-            return query;
-        }
-
-        var searchTerm = search.Search.Trim().ToLowerInvariant();
-        var parameter = Expression.Parameter(typeof(TEntity), "entity");
-        Expression? body = null;
-
-        foreach (var property in stringProperties)
-        {
-            var memberExpression = Expression.Property(parameter, property);
-            var notNullExpression = Expression.NotEqual(memberExpression, Expression.Constant(null, typeof(string)));
-            var lowerExpression = Expression.Call(memberExpression, nameof(string.ToLower), Type.EmptyTypes);
-            var containsExpression = Expression.Call(lowerExpression, nameof(string.Contains), Type.EmptyTypes, Expression.Constant(searchTerm));
-            var safeContainsExpression = Expression.AndAlso(notNullExpression, containsExpression);
-            body = body is null ? safeContainsExpression : Expression.OrElse(body, safeContainsExpression);
-        }
-
-        var predicate = Expression.Lambda<Func<TEntity, bool>>(body!, parameter);
-        return query.Where(predicate);
+        return query;
     }
 
-    private IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, TSearch search)
+    protected virtual IReadOnlyDictionary<string, Func<IQueryable<TEntity>, bool, IQueryable<TEntity>>> BuildSortMappings()
     {
-        var sortExpression = BuildSortExpression(search);
-        if (string.IsNullOrWhiteSpace(sortExpression))
+        return new Dictionary<string, Func<IQueryable<TEntity>, bool, IQueryable<TEntity>>>(StringComparer.OrdinalIgnoreCase)
         {
-            return HasPropertyPath(typeof(TEntity), "Id") ? query.OrderBy("Id") : query;
-        }
-
-        return query.OrderBy(sortExpression);
+            ["id"] = (query, isDescending) => isDescending
+                ? query.OrderByDescending(entity => entity.Id)
+                : query.OrderBy(entity => entity.Id)
+        };
     }
 
-    private string BuildSortExpression(TSearch search)
+    protected virtual IQueryable<TEntity> ApplySorting(IQueryable<TEntity> query, TSearch search)
     {
-        if (string.IsNullOrWhiteSpace(search.SortBy))
-        {
-            return string.Empty;
-        }
-
-        var clauses = new List<string>();
-        var mappings = BuildSortMappings();
-        var tokens = search.SortBy.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-
-        foreach (var token in tokens)
-        {
-            var (fieldToken, isDescendingFromToken) = ParseSortToken(token);
-
-            if (!TryResolveSortField(fieldToken, mappings, out var mappedField))
-            {
-                continue;
-            }
-
-            var isDescending = isDescendingFromToken ?? search.SortDirection == SortDirection.Desc;
-            clauses.Add($"{mappedField} {(isDescending ? "descending" : "ascending")}");
-        }
-
-        return string.Join(", ", clauses);
-    }
-
-    private static (string Field, bool? IsDescending) ParseSortToken(string token)
-    {
-        var trimmed = token.Trim();
-        if (trimmed.StartsWith('-'))
-        {
-            return (trimmed[1..], true);
-        }
-
-        var parts = trimmed.Split(':', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        if (parts.Length == 2)
-        {
-            var direction = parts[1].Equals("desc", StringComparison.OrdinalIgnoreCase) ||
-                            parts[1].Equals("descending", StringComparison.OrdinalIgnoreCase);
-            return (parts[0], direction);
-        }
-
-        return (trimmed, null);
-    }
-
-    private static bool TryResolveSortField(string field, IReadOnlyDictionary<string, string> mappings, out string resolvedField)
-    {
-        if (mappings.TryGetValue(field, out var mappedField))
-        {
-            resolvedField = mappedField;
-            return true;
-        }
-
-        if (HasPropertyPath(typeof(TEntity), field))
-        {
-            resolvedField = field;
-            return true;
-        }
-
-        resolvedField = string.Empty;
-        return false;
-    }
-
-    private static bool HasPropertyPath(Type rootType, string propertyPath)
-    {
-        var type = rootType;
-        var segments = propertyPath.Split('.', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        if (segments.Length == 0)
-        {
-            return false;
-        }
-
-        foreach (var segment in segments)
-        {
-            var property = type.GetProperty(segment);
-            if (property is null)
-            {
-                return false;
-            }
-
-            type = property.PropertyType;
-        }
-
-        return true;
+        return QueryableSorting.Apply(query, search, BuildSortMappings());
     }
 }
