54bf5542e5de860daac99133fef651f4460f6b0b
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-02 21:19:06 +0200

    Remove duplicated code

--- FitBook.WebAPI/Controllers/UserAccountsController.cs
+++ FitBook.WebAPI/Controllers/UserAccountsController.cs
@@ -1,5 +1,4 @@
 using FitBook.Model.Requests.UserAccounts;
-using FitBook.Model.Responses;
 using FitBook.Model.Responses.UserAccounts;
 using FitBook.Model.SearchObjects;
 using FitBook.Services;
@@ -21,37 +20,6 @@ public class UserAccountsController
         : base(service)
     {
     }
-
-    [HttpGet]
-    [ProducesResponseType(typeof(PageResult<UserAccountResponse>), StatusCodes.Status200OK)]
-    public override Task<ActionResult<PageResult<UserAccountResponse>>> GetAll([FromQuery] UserSearchObject search, CancellationToken cancellationToken = default)
-        => base.GetAll(search, cancellationToken);
-
-    [HttpGet("{id:int}")]
-    [ProducesResponseType(typeof(UserAccountResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public override Task<ActionResult<UserAccountResponse>> GetById(int id, CancellationToken cancellationToken = default)
-        => base.GetById(id, cancellationToken);
-
-    [HttpPost]
-    [ProducesResponseType(typeof(UserAccountResponse), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public override Task<ActionResult<UserAccountResponse>> Insert([FromBody] UserAccountInsertRequest request, CancellationToken cancellationToken = default)
-        => base.Insert(request, cancellationToken);
-
-    [HttpPut("{id:int}")]
-    [ProducesResponseType(typeof(UserAccountResponse), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public override Task<ActionResult<UserAccountResponse>> Update(int id, [FromBody] UserAccountUpdateRequest request, CancellationToken cancellationToken = default)
-        => base.Update(id, request, cancellationToken);
-
-    [HttpDelete("{id:int}")]
-    [ProducesResponseType(StatusCodes.Status204NoContent)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public override Task<IActionResult> Delete(int id, CancellationToken cancellationToken = default)
-        => base.Delete(id, cancellationToken);
 
     [HttpPut("{id:int}/password/admin-reset")]
     [Authorize(Roles = "Admin")]
