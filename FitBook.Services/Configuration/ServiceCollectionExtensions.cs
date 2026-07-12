e84310011bfc874b89b196e2f5254ff3ca0dd7ac
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-12 20:44:34 +0200

    Inject all services

--- FitBook.Services/Configuration/ServiceCollectionExtensions.cs
+++ FitBook.Services/Configuration/ServiceCollectionExtensions.cs
@@ -3,12 +3,15 @@ using FitBook.Services.Validators;
 using FluentValidation;
 using FitBook.Model.Requests.UserAccounts;
 using FitBook.Model.Requests.Reservations;
+using FitBook.Model.Requests.MembershipPackages;
+using FitBook.Model.Requests.UserMemberships;
 using Mapster;
 using MapsterMapper;
 using Microsoft.Extensions.DependencyInjection;
 using FitBook.Services.Interfaces.Auth;
 using FitBook.Services.Auth;
 using FitBook.Services.Interfaces;
+using FitBook.Services.Payments;
 
 namespace FitBook.Services.Configuration;
 
@@ -18,6 +21,8 @@ public static class ServiceCollectionExtensions
     {
         var mapsterConfig = TypeAdapterConfig.GlobalSettings;
         mapsterConfig.Scan(typeof(UserAccountMappingConfig).Assembly);
+        mapsterConfig.Scan(typeof(MembershipPackageMappingConfig).Assembly);
+        mapsterConfig.Scan(typeof(UserMembershipMappingConfig).Assembly);
 
         services.AddSingleton(mapsterConfig);
         services.AddScoped<IMapper, ServiceMapper>();
@@ -25,6 +30,9 @@ public static class ServiceCollectionExtensions
         // Domain services
         services.AddScoped<IReservationService, ReservationService>();
         services.AddScoped<IUserAccountService, UserAccountService>();
+        services.AddScoped<IMembershipPackageService, MembershipPackageService>();
+        services.AddScoped<IUserMembershipService, UserMembershipService>();
+        services.AddScoped<IStripePaymentService, StripePaymentService>();
         services.AddScoped<IJwtTokenService, JwtTokenService>();
         services.AddScoped<IRefreshTokenService, RefreshTokenService>();
         services.AddScoped<IAuthService, AuthService>();
@@ -40,6 +48,15 @@ public static class ServiceCollectionExtensions
         services.AddScoped<IValidator<ReservationInsertRequest>, ReservationInsertRequestValidator>();
         services.AddScoped<IValidator<ReservationUpdateRequest>, NullReservationUpdateRequestValidator>();
         services.AddScoped<IValidator<ReservationCancelRequest>, ReservationCancelRequestValidator>();
+
+        // MembershipPackage validators
+        services.AddScoped<IValidator<MembershipPackageInsertRequest>, MembershipPackageInsertRequestValidator>();
+        services.AddScoped<IValidator<MembershipPackageUpdateRequest>, MembershipPackageUpdateRequestValidator>();
+
+        // UserMembership validators
+        services.AddScoped<IValidator<UserMembershipInsertRequest>, UserMembershipInsertRequestValidator>();
+        services.AddScoped<IValidator<UserMembershipUpdateRequest>, NullUserMembershipUpdateRequestValidator>();
+        services.AddScoped<IValidator<UserMembershipCancelRequest>, UserMembershipCancelRequestValidator>();
 
         return services;
     }
