9d73bf2eba3bb2099bf8ad273a9f6a0c71bed82c
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-13 20:14:34 +0200

    Add controller

---
+++ FitBook.WebAPI/Controllers/DifficultyLevelsController.cs
@@ -0,0 +1,37 @@
+using FitBook.Model.Constants;
+using FitBook.Model.Requests.DifficultyLevels;
+using FitBook.Model.Responses.DifficultyLevels;
+using FitBook.Model.SearchObjects;
+using FitBook.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitBook.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class DifficultyLevelsController : BaseCRUDController<DifficultyLevelResponse, DifficultyLevelSearchObject, DifficultyLevelInsertRequest, DifficultyLevelUpdateRequest, IDifficultyLevelService>
+{
+    public DifficultyLevelsController(IDifficultyLevelService service) : base(service)
+    {
+    }
+
+    [Authorize(Roles = Roles.Admin)]
+    public override Task<ActionResult<DifficultyLevelResponse>> Insert([FromBody] DifficultyLevelInsertRequest request, CancellationToken cancellationToken = default)
+    {
+        return base.Insert(request, cancellationToken);
+    }
+
+    [Authorize(Roles = Roles.Admin)]
+    public override Task<ActionResult<DifficultyLevelResponse>> Update(int id, [FromBody] DifficultyLevelUpdateRequest request, CancellationToken cancellationToken = default)
+    {
+        return base.Update(id, request, cancellationToken);
+    }
+
+    [Authorize(Roles = Roles.Admin)]
+    public override Task<IActionResult> Delete(int id, CancellationToken cancellationToken = default)
+    {
+        return base.Delete(id, cancellationToken);
+    }
+}
