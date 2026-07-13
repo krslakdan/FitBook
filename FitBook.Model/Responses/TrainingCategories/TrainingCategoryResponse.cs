d6e5f0620c428a2d76a933900d5b9ef20ae0b307
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-13 20:19:27 +0200

    add request/response, search object, validator

---
+++ FitBook.Model/Responses/TrainingCategories/TrainingCategoryResponse.cs
@@ -0,0 +1,11 @@
+namespace FitBook.Model.Responses.TrainingCategories;
+
+public class TrainingCategoryResponse : IEntityResponse
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+}
