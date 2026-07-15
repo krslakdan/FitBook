4ffb195a23ee5977a219142c427627c724391fc8
krslakdan <191279454+krslakdan@users.noreply.github.com>
2026-07-15 14:17:13 +0200

    add new migration

---
+++ FitBook.Services/Migrations/20260715115527_FixSeedPaymentCurrency.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitBook.Services.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSeedPaymentCurrency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MembershipPayments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Currency",
+                value: "USD");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "MembershipPayments",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Currency",
+                value: "BAM");
+        }
+    }
+}
